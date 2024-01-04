e0c2f04aeaeb4f4efde62983649f499e5d8dbf19
Kench Socorro <kenchsoc@oa-j6.msk.21-school.ru>
2024-01-04 22:35:02 +0300

    parsing

--- src/sprintf.h
+++ src/sprintf.h
@@ -1,8 +1,25 @@
 #ifndef SPRINTF
 #define SPRINTF
 
+#define ON 1
+#define OFF 0
+
+struct Flags {
+    int plus;
+    int minus;
+    int space;
+    int width;
+    int precision;
+    int is_precision;
+    int length_h;
+    int length_l;
+};
 
 int s21_sprintf(char *buf, const char *format, ...);
-
+void record_arg(const char **format, char **str, struct Flags *flags, va_list vl);
+void Obnull(struct Flags *flags);
+int parsing (const char **format, struct Flags *flags);
+int IsDigit(char c);
+int IsSpecifier (char c);
 
 #endif
